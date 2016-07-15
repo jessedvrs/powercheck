ba83acd0360f0c13f1cba90541e3d45d2838a091
Jesse de Vries <jessedvrs@gmail.com>
2016-07-15 11:25:40 +0200

    fix(getErrorClue): fix typos in error messages

--- src/lib/helpers/getErrorClue.js
+++ src/lib/helpers/getErrorClue.js
@@ -15,12 +15,12 @@ export default function getErrorClue(value, result) {
         case 'wrapper.equals':
             return `The given value "${value}" did not strictly equal "${result.info.expectedValue}".`;
         case 'nativetype':
-            return `Expected${result.optional ? `optional ` : ``} ${result.info.expectedType}, got ${result.info.got}.`;
+            return `Expected${result.optional ? ` optional` : ``} ${result.info.expectedType}, got ${result.info.got}.`;
         case 'instanceof':
-            return `Expected${result.optional ? `optional ` : ``} instance of ${result.info.expectedInstance}, got ${result.info.got}.`;
+            return `Expected${result.optional ? ` optional` : ``} instance of ${result.info.expectedInstance}, got ${result.info.got}.`;
         case 'array-literal':
-            return `Expected${result.optional ? `optional ` : ``} array, got ${result.info.got}.`;
+            return `Expected${result.optional ? ` optional` : ``} array, got ${result.info.got}.`;
         case 'object-literal':
-            return `Expected${result.optional ? `optional ` : ``} object, got ${result.info.got}.`;
+            return `Expected${result.optional ? ` optional` : ``} object, got ${result.info.got}.`;
     }
 };
